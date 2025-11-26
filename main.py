77f2b6bd15d45c98472a5a7b410eeee335d82a87
Tate <tatesinclairwork@gmail.com>
2025-11-26 23:34:13 +0000

    Final changes for the night of (26/11/2025)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
-from api_functions import get_token, search_for_album, get_album_details
+from api_functions import get_token, search_for_album, get_album_details, ms_to_min_sec
 import streamlit as st
+import pandas as pd
 import os
 import json
 
@@ -34,16 +35,24 @@ if __name__ == "__main__":
             url = data['images'][1]['url']
             Album_link = data['external_urls']['spotify']
             Album_id = data['id']
+            Album_name = data["name"]
 
             data = get_album_details(album_id=Album_id, token=token)
+            tracks = data['tracks']['items']
 
-            with open('info.json', 'w+') as file:
-                json.dump(data, file, indent=4)
-            print(data)
+            df = pd.DataFrame({
+                "Name": [t["name"] for t in tracks],
+                "Duration (ms)": [ms_to_min_sec(t['duration_ms']) for t in tracks]
+            })
+
         except Exception as e:
             st.warning(e)
             
-        st.subheader(Artist_Name)
-        st.write("Album cover")
-        st.image(url, width=300)
-        st.markdown(f"[View the album]({Album_link})")
+        with col3:
+            st.header(Artist_Name)
+            st.subheader(Album_name)
+            st.image(url, width=300)
+            st.markdown(f"[View the album]({Album_link})")
+
+        with col4:
+            st.dataframe(df)
